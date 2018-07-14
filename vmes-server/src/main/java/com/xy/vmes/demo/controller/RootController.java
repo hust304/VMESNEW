4a8553239fb29591776d7ece54cce91c8447ecff
luoyifan <luoyifan@qq.com>
2018-07-14 15:33:39 +0800

    init2

--- vmes-server/src/main/java/com/xy/vmes/demo/controller/RootController.java
+++ vmes-server/src/main/java/com/xy/vmes/demo/controller/RootController.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class RootController {
 
+    private Long a;
+
     @Autowired
     private DepartmentService departmentService;
 
