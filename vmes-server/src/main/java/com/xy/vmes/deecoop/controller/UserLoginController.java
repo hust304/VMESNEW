14b5cc8a6f961d2f5e658beff899fa357121fcac
ChenGang7906 <chen_gang7906@163.com>
2018-07-24 16:03:25 +0800

    用户登录验证码生成与验证

--- vmes-server/src/main/java/com/xy/vmes/deecoop/controller/UserLoginController.java
+++ vmes-server/src/main/java/com/xy/vmes/deecoop/controller/UserLoginController.java
@@ -16,10 +16,30 @@ import org.springframework.web.bind.annotation.RestController;
 import redis.clients.jedis.Jedis;
 import com.google.gson.Gson;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Random;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 /**
  * 说明：用户登录退出系统 Controller
@@ -57,7 +77,7 @@ public class UserLoginController {
      *
      * 返回值: JsonString
      * {
-     *     result:Success 成功-fail 失败
+     *     result:success 成功-fail 失败
      *     msg:成功或失败信息
      *     RedisUuid: 当前Redis会话ID
      *     RedisKey: Redis缓存Key(uuid:用户ID:deecoop)
@@ -81,10 +101,25 @@ public class UserLoginController {
             if (pageData.get("userPassword") == null || pageData.get("userPassword").toString().trim().length() == 0 ) {
                 msgBuf.append("参数错误：密码输入为空或空字符串，密码为必填项不可为空！</br>");
             }
+
+            if (pageData.get("securityCode") == null || pageData.get("securityCode").toString().trim().length() == 0 ) {
+                msgBuf.append("参数错误：验证码入为空或空字符串，验证码为必填项不可为空！</br>");
+            }
         }
 
         if (msgBuf.toString().trim().length() > 0) {
             throw new RestException("", msgBuf.toString());
+        }
+
+        //验证码-是否过期
+        Jedis jedis = redisClient.getJedisPool().getResource();
+        if (jedis == null) {
+            throw new RestException("", "Redis 缓存错误(jedis is null)，请与管理员联系！");
+        }
+        String securityCode = pageData.get("securityCode").toString().trim();
+        String old_securityCode = redisClient.get(com.yvan.common.Common.REDIS_SECURITY_CODE);
+        if (!securityCode.equalsIgnoreCase(old_securityCode)) {
+            throw new RestException("", "验证码已经过期，请重新输入验证码！");
         }
 
         //1. (用户账号, 密码MD5)-
@@ -96,10 +131,6 @@ public class UserLoginController {
         Map<String, Object> userEmployMap = objectList.get(0);
         String userID = userEmployMap.get("userID").toString().toLowerCase();
 
-        Jedis jedis = redisClient.getJedisPool().getResource();
-        if (jedis == null) {
-            throw new RestException("", "Redis 缓存错误(jedis is null)，请与管理员联系！");
-        }
 
         //(用户账号, 密码MD5)-系统中存在--RedisKey: uuid_系统用户ID_deecoop
         //3. 生成新的Redis会话(生成新的uuid)
@@ -140,5 +171,112 @@ public class UserLoginController {
         //RedisUserLoginKey(用户-员工-角色Map)
 
         return null;
+    }
+
+    /**
+     * 1. 生成4位验证码
+     * 2. 验证码-Redis缓存Key:(securityCode)
+     * 3. 验证码-返回页面
+     * 返回值: JsonString
+     * {
+     *    securityCode: 验证码
+     * }
+     * 创建人：陈刚
+     * 创建时间：2018-07-24
+     */
+    @GetMapping("/userLogin/createSecurityCode")
+    public String createSecurityCode() {
+        HttpServletResponse httpResponse = HttpUtils.currentResponse();
+
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        String SecurityCode = drawImg(output);
+
+        Jedis jedis = redisClient.getJedisPool().getResource();
+        if (jedis == null) {
+            throw new RestException("", "Redis 缓存错误(jedis is null)，请与管理员联系！");
+        }
+
+        //Redis-验证码-缓存1分钟(60 * 1000)
+        redisClient.setWithExpireTime(com.yvan.common.Common.REDIS_SECURITY_CODE, SecurityCode, 1 * 60 * 1000);
+
+        Map<String, String> mapObj = new HashMap<String, String>();
+        mapObj.put("result", "success");
+        mapObj.put("securityCode", SecurityCode);
+
+        if (mapObj != null && mapObj.size() > 0) {
+            return new Gson().toJson(mapObj);
+        }
+
+        return new String();
+    }
+
+
+
+    ////////////////////////////////////////////////////////////////////////////
+    //测试代码
+    @GetMapping("/userLogin/sysToApplicationPage")
+    public String sysToApplicationPage() {
+        PageData pageData = HttpUtils.parsePageData();
+
+        //检查是否sessionId，且sessionId是否过期
+        String RedisUuid = pageData.get("RedisUuid").toString().trim();
+        String userID = pageData.get("userID").toString().trim();
+
+        if (RedisUuid == null || RedisUuid.trim().length() == 0) {}
+        if (userID == null || userID.trim().length() == 0) {}
+
+        RedisUuid = RedisUuid.toLowerCase();
+        userID = userID.toLowerCase();
+
+        Jedis jedis = redisClient.getJedisPool().getResource();
+        if (jedis == null) {
+            throw new RestException("", "Redis 缓存错误(jedis is null)，请与管理员联系！");
+        }
+
+        String HistoryRedisUuid = redisClient.findRedisUuidByUserID(jedis, userID);
+        if (HistoryRedisUuid == null || HistoryRedisUuid.trim().length() == 0) {
+            throw new RestException("", "userID:" + userID + " 没有登录！");
+        } else if (!HistoryRedisUuid.toLowerCase().equals(RedisUuid)) {
+            throw new RestException("", "userID:" + userID + " 会话已经过期！");
+        }
+
+        return "true";
+    }
+
+    private String drawImg(ByteArrayOutputStream output){
+        String code = "";
+        for(int i=0; i<4; i++){
+            code += randomChar();
+        }
+        int width = 70;
+        int height = 25;
+        BufferedImage bi = new BufferedImage(width,height,BufferedImage.TYPE_3BYTE_BGR);
+        Font font = new Font("Times New Roman",Font.PLAIN,20);
+        Graphics2D g = bi.createGraphics();
+        g.setFont(font);
+        Color color = new Color(66,2,82);
+        g.setColor(color);
+        g.setBackground(new Color(226,226,240));
+        g.clearRect(0, 0, width, height);
+        FontRenderContext context = g.getFontRenderContext();
+        Rectangle2D bounds = font.getStringBounds(code, context);
+        double x = (width - bounds.getWidth()) / 2;
+        double y = (height - bounds.getHeight()) / 2;
+        double ascent = bounds.getY();
+        double baseY = y - ascent;
+        g.drawString(code, (int)x, (int)baseY);
+        g.dispose();
+        try {
+            ImageIO.write(bi, "jpg", output);
+        } catch (IOException e) {
+            //e.printStackTrace();
+        }
+        return code;
+    }
+
+    private char randomChar(){
+        Random r = new Random();
+        String s = "ABCDEFGHJKLMNPRSTUVWXYZ0123456789";
+        return s.charAt(r.nextInt(s.length()));
     }
 }
--- yvan-platform/src/main/java/com/yvan/common/Common.java
+++ yvan-platform/src/main/java/com/yvan/common/Common.java
@@ -1,13 +1,14 @@
 package com.yvan.common;
 
 public class Common {
+    public final static String REDIS_SECURITY_CODE = "securityCode";
     public final static String REDIS_USER_ID = "userID";
     public final static String REDIS_USER_CODE = "userCode";
     public final static String REDIS_COMPANYID = "companyID";
     public final static String REDIS_EMPLOYID = "employID";
     public final static String REDIS_EMPLOYNAME = "employName";
     public final static String REDIS_DEPTID = "deptID";
-    public final static String REDIS_POSTID = "postid";
+    public final static String REDIS_POSTID = "postID";
     public final static String REDIS_ROLES = "roles";
     public final static String REDIS_USERMENUTREE = "userMenuTree";
     public final static String REDIS_USERLOGINMAP = "userLoginMap";
