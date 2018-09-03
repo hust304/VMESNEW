0ed69d9ea6230c7b545384edb0d56a4cc1a50da2
ChenGang7906 <chen_gang7906@163.com>
2018-09-03 16:23:15 +0800

    员工管理-岗位设置功能

--- vmes-server/src/main/java/com/xy/vmes/deecoop/system/controller/EmployeeController.java
+++ vmes-server/src/main/java/com/xy/vmes/deecoop/system/controller/EmployeeController.java
@@ -772,7 +772,7 @@ public class EmployeeController {
      */
     @GetMapping("/employee/addEmployeeMainPost")
     public ResultModel addEmployeeMainPost()  throws Exception {
-        logger.info("################employee/addEmployeeMasterPost 执行开始 ################# ");
+        logger.info("################employee/addEmployeeMainPost 执行开始 ################# ");
         Long startTime = System.currentTimeMillis();
 
         ResultModel model = new ResultModel();
@@ -822,7 +822,7 @@ public class EmployeeController {
         employPostService.save(newEmployPost);
 
         Long endTime = System.currentTimeMillis();
-        logger.info("################employee/addEmployeeMasterPost 执行结束 总耗时"+(endTime-startTime)+"ms ################# ");
+        logger.info("################employee/addEmployeeMainPost 执行结束 总耗时"+(endTime-startTime)+"ms ################# ");
         return model;
     }
 
