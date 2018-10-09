6443b954e8bbc491c2db7b9b0b14701e2e45d307
ChenGang7906 <chen_gang7906@163.com>
2018-10-09 15:53:11 +0800

    仓库管理-仓库货位功能

---
+++ vmes-contracts/src/main/java/com/xy/vmes/service/WarehousePositionService.java
@@ -0,0 +1,87 @@
+package com.xy.vmes.service;
+
+import com.baomidou.mybatisplus.plugins.pagination.Pagination;
+import com.xy.vmes.entity.Department;
+import com.xy.vmes.entity.WarehousePosition;
+import com.yvan.PageData;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+* 说明：vmes_warehouse_position:仓库货位表 接口类
+* 创建人：陈刚 自动生成
+* 创建时间：2018-10-09
+*/
+public interface WarehousePositionService {
+
+    /**
+    * 创建人：陈刚 自动创建，禁止修改
+    * 创建时间：2018-10-09
+    */
+    void save(WarehousePosition warehousePosition) throws Exception;
+
+    /**
+    * 创建人：陈刚 自动创建，禁止修改
+    * 创建时间：2018-10-09
+    */
+    void update(WarehousePosition warehousePosition) throws Exception;
+
+    /**
+    * 创建人：陈刚 自动创建，禁止修改
+    * 创建时间：2018-10-09
+    */
+    void updateAll(WarehousePosition warehousePosition) throws Exception;
+
+    /**
+    * 创建人：陈刚 自动创建，禁止修改
+    * 创建时间：2018-10-09
+    */
+    void deleteById(String id) throws Exception;
+
+    /**
+    * 创建人：陈刚 自动创建，禁止修改
+    * 创建时间：2018-10-09
+    */
+    void deleteByIds(String[] ids) throws Exception;
+
+    /**
+    * 创建人：陈刚 自动创建，禁止修改
+    * 创建时间：2018-10-09
+    */
+    List<WarehousePosition> dataList(PageData pd) throws Exception;
+
+    /**
+    * 创建人：陈刚 自动创建，禁止修改
+    * 创建时间：2018-10-09
+    */
+    void deleteByColumnMap(Map columnMap) throws Exception;
+
+    /**
+    * 创建人：陈刚 自动创建，禁止修改
+    * 创建时间：2018-10-09
+    */
+    List<Map> getDataListPage(PageData pd,Pagination pg) throws Exception;
+
+    /**
+    * 创建人：陈刚 自动创建，禁止修改
+    * 创建时间：2018-10-09
+    */
+    void updateToDisableByIds(String[] ids)throws Exception;
+
+    /*****************************************************以上为自动生成代码禁止修改，请在下面添加业务代码**************************************************/
+    WarehousePosition findWarehousePosition(PageData object);
+    WarehousePosition findWarehousePositionById(String id);
+
+    List<WarehousePosition> findWarehousePositionList(PageData object);
+    List<WarehousePosition> findWarehousePositionListByPid(String pid);
+
+    boolean isExistByName(String pid, String id, String name);
+
+    List<String> findNameListByNumber(int start, int end);
+    List<String> findNameListByChar(String start, String end);
+    List<String> findNameList(String start, String end, String suffix);
+}
+
+
+
