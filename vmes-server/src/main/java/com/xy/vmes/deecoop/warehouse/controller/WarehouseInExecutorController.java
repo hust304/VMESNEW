a80627e983b90e2a4dd43309a8510b48b9cad6ee
ChenGang7906 <chen_gang7906@163.com>
2018-11-05 18:32:16 +0800

    入库管理

---
+++ vmes-server/src/main/java/com/xy/vmes/deecoop/warehouse/controller/WarehouseInExecutorController.java
@@ -0,0 +1,106 @@
+package com.xy.vmes.deecoop.warehouse.controller;
+
+import com.baomidou.mybatisplus.plugins.pagination.Pagination;
+import com.xy.vmes.entity.Column;
+import com.xy.vmes.service.ColumnService;
+import com.xy.vmes.service.WarehouseInExecutorService;
+import com.yvan.HttpUtils;
+import com.yvan.PageData;
+import com.yvan.springmvc.ResultModel;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+
+/**
+ * 说明：入库派单明细执行
+ * @author 陈刚 自动生成
+ * @date 2018-10-18
+ */
+@RestController
+@Slf4j
+public class WarehouseInExecutorController {
+    private Logger logger = LoggerFactory.getLogger(WarehouseInExecutorController.class);
+
+    @Autowired
+    private WarehouseInExecutorService warehouseInExecutorService;
+    @Autowired
+    private ColumnService columnService;
+
+    /**
+     * @author 陈刚 自动创建，可以修改
+     * @date 2018-10-16
+     */
+    @PostMapping("/warehouseInExecutor/listPageWarehouseInExecutor")
+    public ResultModel listPageWarehouseInExecutor() throws Exception {
+        logger.info("################warehouseInExecutor/listPageWarehouseInExecutor 执行开始 ################# ");
+        Long startTime = System.currentTimeMillis();
+        ResultModel model = new ResultModel();
+
+        List<Column> columnList = columnService.findColumnList("WarehouseInExecutor");
+        if (columnList == null || columnList.size() == 0) {
+            model.putCode("1");
+            model.putMsg("数据库没有生成TabCol，请联系管理员！");
+            return model;
+        }
+
+        //获取指定栏位字符串-重新调整List<Column>
+        PageData pd = HttpUtils.parsePageData();
+        String fieldCode = pd.getString("fieldCode");
+        if (fieldCode != null && fieldCode.trim().length() > 0) {
+            columnList = columnService.modifyColumnByFieldCode(fieldCode, columnList);
+        }
+
+        List<LinkedHashMap> titlesList = new ArrayList<LinkedHashMap>();
+        List<String> titlesHideList = new ArrayList<String>();
+        Map<String, String> varModelMap = new HashMap<String, String>();
+        if(columnList!=null&&columnList.size()>0){
+            for (Column column : columnList) {
+                if(column!=null){
+                    if("0".equals(column.getIshide())){
+                        titlesHideList.add(column.getTitleKey());
+                    }
+                    LinkedHashMap titlesLinkedMap = new LinkedHashMap();
+                    titlesLinkedMap.put(column.getTitleKey(),column.getTitleName());
+                    varModelMap.put(column.getTitleKey(),"");
+                    titlesList.add(titlesLinkedMap);
+                }
+            }
+        }
+        Map result = new HashMap();
+        result.put("hideTitles",titlesHideList);
+        result.put("titles",titlesList);
+
+        pd.put("orderStr", "executor.cdate asc");
+        String orderStr = pd.getString("orderStr");
+        if (orderStr != null && orderStr.trim().length() > 0) {
+            pd.put("orderStr", orderStr);
+        }
+
+        Pagination pg = HttpUtils.parsePagination(pd);
+        List<Map> varMapList = new ArrayList();
+        List<Map> varList = warehouseInExecutorService.getDataListPage(pd, pg);
+        if(varList!=null&&varList.size()>0){
+            for(int i=0;i<varList.size();i++){
+                Map map = varList.get(i);
+                Map<String, String> varMap = new HashMap<String, String>();
+                varMap.putAll(varModelMap);
+                for (Map.Entry<String, String> entry : varMap.entrySet()) {
+                    varMap.put(entry.getKey(),map.get(entry.getKey())!=null?map.get(entry.getKey()).toString():"");
+                }
+                varMapList.add(varMap);
+            }
+        }
+        result.put("varList",varMapList);
+        result.put("pageData", pg);
+
+        model.putResult(result);
+        Long endTime = System.currentTimeMillis();
+        logger.info("################warehouseInExecutor/listPageWarehouseInExecutor 执行结束 总耗时"+(endTime-startTime)+"ms ################# ");
+        return model;
+    }
+}
