43e57d3f786928bc72d813afca11b235f434e994
ChenGang7906 <chen_gang7906@163.com>
2019-01-22 09:52:32 +0800

    订单明细字段修改

--- vmes-contracts/src/main/java/com/xy/vmes/entity/SaleOrderDetail.java
+++ vmes-contracts/src/main/java/com/xy/vmes/entity/SaleOrderDetail.java
@@ -22,7 +22,7 @@ public class SaleOrderDetail implements Serializable {
 	//订单ID
 	@TableField("parent_id")
 	private String parentId;
-	//订单状态(0:待提交 1:待审核 2:待生产 3:待出库 4:待发货 5:已发货 6:已完成 -1:已取消)
+	//订单状态(0:待提交 1:待审核 2:待出库 3:待发货 4:已发货 5:已完成 -1:已取消)
 	@TableField("state")
 	private String state;
 	//附件地址
@@ -32,12 +32,25 @@ public class SaleOrderDetail implements Serializable {
 	@TableField("deliver_date")
 	private Date deliverDate;
 
-	//订购数量(计价数量)
-	@TableField("count")
-	private BigDecimal count;
+	//订单订购数量
+	@TableField("order_count")
+	private BigDecimal orderCount;
+	//订单单位id
+	@TableField("order_unit")
+	private String orderUnit;
 	//货品数量(计量数量)
 	@TableField("product_count")
 	private BigDecimal productCount;
+	//计量单位id
+	@TableField("product_unit")
+	private String productUnit;
+	//货品数量(计价数量)
+	@TableField("price_count")
+	private BigDecimal priceCount;
+
+	//计价单位id
+	@TableField("price_unit")
+	private String priceUnit;
 	//货品ID
 	@TableField("product_id")
 	private String productId;
@@ -47,39 +60,29 @@ public class SaleOrderDetail implements Serializable {
 	//货品金额(订购数量 * 货品单价)
 	@TableField("product_sum")
 	private BigDecimal productSum;
-
-	//计量单位id
-	@TableField("product_unit")
-	private String productUnit;
-	//计价单位id
-	@TableField("price_unit")
-	private String priceUnit;
-	//是否锁定仓库(0:未锁定 1:已锁定
+	//是否锁定仓库(0:未锁定 1:已锁定)
 	@TableField("is_lock_warehouse")
 	private String isLockWarehouse;
+
 	//是否需要生产(0:无需 1:需要 库存不够)
 	@TableField("is_need_produce")
 	private String isNeedProduce;
 	//锁定货品数量
 	@TableField("lock_count")
 	private BigDecimal lockCount;
-
 	//锁定开始时间
 	@TableField("lock_date")
 	private Date lockDate;
-	//发货明细ID
-	@TableField("deliver_detail_id")
-	private String deliverDetailId;
 	//生产计划明细ID
 	@TableField("plan_detail_id")
 	private String planDetailId;
 	//创建用户id
 	@TableField("cuser")
 	private String cuser;
+
 	//创建时间
 	@TableField("cdate")
 	private Date cdate;
-
 	//修改用户id
 	@TableField("uuser")
 	private String uuser;
@@ -92,6 +95,7 @@ public class SaleOrderDetail implements Serializable {
 	//是否启用(0:已禁用 1:启用)
 	@TableField("isdisable")
 	private String isdisable;
+
 
 	public void setId(String id) {
 		this.id = id;
@@ -123,17 +127,41 @@ public class SaleOrderDetail implements Serializable {
 	public Date getDeliverDate() {
 		return deliverDate;
 	}
-	public void setCount(BigDecimal count) {
-		this.count = count;
+	public void setOrderCount(BigDecimal orderCount) {
+		this.orderCount = orderCount;
 	}
-	public BigDecimal getCount() {
-		return count;
+	public BigDecimal getOrderCount() {
+		return orderCount;
+	}
+	public void setOrderUnit(String orderUnit) {
+		this.orderUnit = orderUnit;
+	}
+	public String getOrderUnit() {
+		return orderUnit;
 	}
 	public void setProductCount(BigDecimal productCount) {
 		this.productCount = productCount;
 	}
 	public BigDecimal getProductCount() {
 		return productCount;
+	}
+	public void setProductUnit(String productUnit) {
+		this.productUnit = productUnit;
+	}
+	public String getProductUnit() {
+		return productUnit;
+	}
+	public void setPriceCount(BigDecimal priceCount) {
+		this.priceCount = priceCount;
+	}
+	public BigDecimal getPriceCount() {
+		return priceCount;
+	}
+	public void setPriceUnit(String priceUnit) {
+		this.priceUnit = priceUnit;
+	}
+	public String getPriceUnit() {
+		return priceUnit;
 	}
 	public void setProductId(String productId) {
 		this.productId = productId;
@@ -153,29 +181,17 @@ public class SaleOrderDetail implements Serializable {
 	public BigDecimal getProductSum() {
 		return productSum;
 	}
-	public void setProductUnit(String productUnit) {
-		this.productUnit = productUnit;
-	}
-	public String getProductUnit() {
-		return productUnit;
-	}
-	public void setPriceUnit(String priceUnit) {
-		this.priceUnit = priceUnit;
-	}
-	public String getPriceUnit() {
-		return priceUnit;
-	}
 	public void setIsLockWarehouse(String isLockWarehouse) {
 		this.isLockWarehouse = isLockWarehouse;
 	}
 	public String getIsLockWarehouse() {
 		return isLockWarehouse;
 	}
-	public String getIsNeedProduce() {
-		return isNeedProduce;
-	}
 	public void setIsNeedProduce(String isNeedProduce) {
 		this.isNeedProduce = isNeedProduce;
+	}
+	public String getIsNeedProduce() {
+		return isNeedProduce;
 	}
 	public void setLockCount(BigDecimal lockCount) {
 		this.lockCount = lockCount;
@@ -189,13 +205,6 @@ public class SaleOrderDetail implements Serializable {
 	public Date getLockDate() {
 		return lockDate;
 	}
-	public void setDeliverDetailId(String outDetailId) {
-		this.deliverDetailId = outDetailId;
-	}
-	public String getDeliverDetailId() {
-		return deliverDetailId;
-	}
-
 	public void setPlanDetailId(String planDetailId) {
 		this.planDetailId = planDetailId;
 	}
--- vmes-contracts/src/main/java/com/xy/vmes/entity/SaleOrderDetailEntity.java
+++ vmes-contracts/src/main/java/com/xy/vmes/entity/SaleOrderDetailEntity.java
@@ -9,46 +9,48 @@ public class SaleOrderDetailEntity {
     private String id;
     //订单ID
     private String parentId;
-    //订单状态(0:待提交 1:待审核 2:待生产 3:待出库 4:待发货 5:已发货 6:已完成 -1:已取消)
+    //订单状态(0:待提交 1:待审核 2:待出库 3:待发货 4:已发货 5:已完成 -1:已取消)
     private String state;
     //附件地址
     private String fileUrl;
     //交付日期
     private Date deliverDate;
 
-    //订购数量(计价数量)
-    private BigDecimal count;
+    //订单订购数量
+    private BigDecimal orderCount;
+    //订单单位id
+    private String orderUnit;
     //货品数量(计量数量)
     private BigDecimal productCount;
+    //计量单位id
+    private String productUnit;
+    //货品数量(计价数量)
+    private BigDecimal priceCount;
+
+    //计价单位id
+    private String priceUnit;
     //货品ID
     private String productId;
     //货品单价(货品计价单位匹配获得)
     private BigDecimal productPrice;
     //货品金额(订购数量 * 货品单价)
     private BigDecimal productSum;
-
-    //计量单位id
-    private String productUnit;
-    //计价单位id
-    private String priceUnit;
-    //是否锁定仓库(0:未锁定 1:已锁定
+    //是否锁定仓库(0:未锁定 1:已锁定)
     private String isLockWarehouse;
+
     //是否需要生产(0:无需 1:需要 库存不够)
     private String isNeedProduce;
     //锁定货品数量
     private BigDecimal lockCount;
-
     //锁定开始时间
     private Date lockDate;
-    //发货明细ID
-    private String deliverDetailId;
     //生产计划明细ID
     private String planDetailId;
     //创建用户id
     private String cuser;
+
     //创建时间
     private Date cdate;
-
     //修改用户id
     private String uuser;
     //修改时间
@@ -59,8 +61,8 @@ public class SaleOrderDetailEntity {
     private String isdisable;
 
     //非实体类字段:界面传值用
-    private String orderUnit;
     private String deliverDtlRemark;
+
 
     public void setId(String id) {
         this.id = id;
@@ -92,17 +94,41 @@ public class SaleOrderDetailEntity {
     public Date getDeliverDate() {
         return deliverDate;
     }
-    public void setCount(BigDecimal count) {
-        this.count = count;
+    public void setOrderCount(BigDecimal orderCount) {
+        this.orderCount = orderCount;
     }
-    public BigDecimal getCount() {
-        return count;
+    public BigDecimal getOrderCount() {
+        return orderCount;
+    }
+    public void setOrderUnit(String orderUnit) {
+        this.orderUnit = orderUnit;
+    }
+    public String getOrderUnit() {
+        return orderUnit;
     }
     public void setProductCount(BigDecimal productCount) {
         this.productCount = productCount;
     }
     public BigDecimal getProductCount() {
         return productCount;
+    }
+    public void setProductUnit(String productUnit) {
+        this.productUnit = productUnit;
+    }
+    public String getProductUnit() {
+        return productUnit;
+    }
+    public void setPriceCount(BigDecimal priceCount) {
+        this.priceCount = priceCount;
+    }
+    public BigDecimal getPriceCount() {
+        return priceCount;
+    }
+    public void setPriceUnit(String priceUnit) {
+        this.priceUnit = priceUnit;
+    }
+    public String getPriceUnit() {
+        return priceUnit;
     }
     public void setProductId(String productId) {
         this.productId = productId;
@@ -122,29 +148,17 @@ public class SaleOrderDetailEntity {
     public BigDecimal getProductSum() {
         return productSum;
     }
-    public void setProductUnit(String productUnit) {
-        this.productUnit = productUnit;
-    }
-    public String getProductUnit() {
-        return productUnit;
-    }
-    public void setPriceUnit(String priceUnit) {
-        this.priceUnit = priceUnit;
-    }
-    public String getPriceUnit() {
-        return priceUnit;
-    }
     public void setIsLockWarehouse(String isLockWarehouse) {
         this.isLockWarehouse = isLockWarehouse;
     }
     public String getIsLockWarehouse() {
         return isLockWarehouse;
     }
-    public String getIsNeedProduce() {
-        return isNeedProduce;
-    }
     public void setIsNeedProduce(String isNeedProduce) {
         this.isNeedProduce = isNeedProduce;
+    }
+    public String getIsNeedProduce() {
+        return isNeedProduce;
     }
     public void setLockCount(BigDecimal lockCount) {
         this.lockCount = lockCount;
@@ -157,12 +171,6 @@ public class SaleOrderDetailEntity {
     }
     public Date getLockDate() {
         return lockDate;
-    }
-    public void setDeliverDetailId(String outDetailId) {
-        this.deliverDetailId = outDetailId;
-    }
-    public String getDeliverDetailId() {
-        return deliverDetailId;
     }
     public void setPlanDetailId(String planDetailId) {
         this.planDetailId = planDetailId;
@@ -205,13 +213,6 @@ public class SaleOrderDetailEntity {
     }
     public String getIsdisable() {
         return isdisable;
-    }
-
-    public String getOrderUnit() {
-        return orderUnit;
-    }
-    public void setOrderUnit(String orderUnit) {
-        this.orderUnit = orderUnit;
     }
     public String getDeliverDtlRemark() {
         return deliverDtlRemark;
