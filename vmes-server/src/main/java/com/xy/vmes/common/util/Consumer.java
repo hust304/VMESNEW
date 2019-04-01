67a0fab68fdcc38f48af66989e25d40f51bb33b9
ChenGang7906 <chen_gang7906@163.com>
2019-04-01 17:17:58 +0800

    消息队列-锁库时间到后-解锁库存

--- vmes-server/src/main/java/com/xy/vmes/common/util/Consumer.java
+++ vmes-server/src/main/java/com/xy/vmes/common/util/Consumer.java
@@ -3,6 +3,7 @@ package com.xy.vmes.common.util;
 import com.xy.vmes.entity.SaleOrderDetail;
 import com.xy.vmes.service.ProductService;
 import com.xy.vmes.service.SaleOrderDetailService;
+import com.yvan.PageData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
@@ -21,21 +22,35 @@ public class Consumer {
     private ProductService productService;
 
     /**
-     * 消息队列接收消息-货品锁定库存解锁
+     * 消息队列接收消息-货品锁定库存解锁-信息队列信息:(订单明细id,锁定库存版本号)
      * 销售-订单管理-订单明细-订单明细货品锁定库存
      *
      * 1. 修改货品锁定库存数量
      * 2. 修改订单明细锁库字段
      *
-     * @param orderDtlId  订单明细id
+     * @param activeMQ_msg  信息队列信息:(订单明细id,锁定库存版本号)
      *
      */
     @JmsListener(destination = "sale.orderDetail.ProductLockcount.queue")
-    public void receiveProductLockcount (String orderDtlId) throws Exception {
-        //System.out.println("ProductLockcount 收到的报文为:" + orderDtlId);
-        if (orderDtlId == null || orderDtlId.trim().length() == 0) {return;}
+    public void receiveProductLockcount (String activeMQ_msg) throws Exception {
+        if (activeMQ_msg == null || activeMQ_msg.trim().length() == 0) {return;}
+        String[] strArray = activeMQ_msg.split(",");
 
-        SaleOrderDetail orderDetail = saleOrderDetailService.findSaleOrderDetailById(orderDtlId);
+        String orderDtlId = "";
+        if (strArray != null && strArray.length >= 1) {
+            orderDtlId = strArray[0];
+        }
+
+        String versionLockCount = "";
+        if (strArray != null && strArray.length >= 2) {
+            versionLockCount = strArray[1];
+        }
+
+        PageData findMap = new PageData();
+        findMap.put("orderDtlId", orderDtlId);
+        findMap.put("versionLockCount", versionLockCount);
+
+        SaleOrderDetail orderDetail = saleOrderDetailService.findSaleOrderDetail(findMap);
         //is_lock_warehouse 是否锁定仓库(0:未锁定 1:已锁定
         if (orderDetail != null && "1".equals(orderDetail.getIsLockWarehouse())) {
             //锁定货品数量
