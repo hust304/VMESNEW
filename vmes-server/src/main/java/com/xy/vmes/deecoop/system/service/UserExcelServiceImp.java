9f43744eb11edeb11bf4a6b0134c5ec2a1076199
ChenGang7906 <chen_gang7906@163.com>
2019-04-09 18:07:03 +0800

    用户管理Excel导入功能

--- vmes-server/src/main/java/com/xy/vmes/deecoop/system/service/UserExcelServiceImp.java
+++ vmes-server/src/main/java/com/xy/vmes/deecoop/system/service/UserExcelServiceImp.java
@@ -3,7 +3,10 @@ package com.xy.vmes.deecoop.system.service;
 import com.xy.vmes.common.util.Common;
 import com.xy.vmes.service.UserExcelService;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.text.MessageFormat;
+import java.text.ParseException;
 import java.util.LinkedHashMap;
 import java.util.List;
 
@@ -27,6 +30,7 @@ public class UserExcelServiceImp implements UserExcelService {
 
 
         String msg_column_isnull = "第 {0} 行: ({1})输入为空或空字符串，({1})是必填字段不可为空！" + Common.SYS_ENDLINE_DEFAULT;
+        String msg_column_mobile_error = "第 {0} 行: {1}:{2} 输入错误，请输入11位手机号码！" + Common.SYS_ENDLINE_DEFAULT;
 
         int maxRow = 0;
         for (int i = 0; i < objectList.size(); i++) {
@@ -74,13 +78,39 @@ public class UserExcelServiceImp implements UserExcelService {
 //            }
 
             //手机号 mobile
-            String userTypeName = mapObject.get("userTypeName");
-            if (userTypeName == null || userTypeName.trim().length() == 0) {
+            String mobile = mapObject.get("mobile");
+            if (mobile == null || mobile.trim().length() == 0) {
                 //String msg_column_isnull = "第 {0} 行: ({1})输入为空或空字符串，({1})是必填字段不可为空！"
                 String str_isnull = MessageFormat.format(msg_column_isnull,
                         (i+index_int),
-                        "用户类型");
+                        "手机号");
                 strBuf.append(str_isnull);
+            } else if (mobile != null && mobile.trim().length() > 0) {
+                try {
+                    new BigInteger(mobile);
+                    if (mobile.trim().length() != 11) {
+                        //String msg_column_mobile_error = "第 {0} 行: {1}:{2} 输入错误，请输入11位手机号码！"
+                        String str_error = MessageFormat.format(msg_column_mobile_error,
+                                (i+index_int),
+                                "手机号",
+                                mobile);
+                        strBuf.append(str_error);
+
+                        maxRow = maxRow + 1;
+                        if (maxShowRow_int <= maxRow) {return strBuf.toString();}
+                    }
+                } catch (NumberFormatException e) {
+                    //e.printStackTrace();
+                    //String msg_column_mobile_error = "第 {0} 行: {1}:{2} 输入错误，请输入11位手机号码！"
+                    String str_error = MessageFormat.format(msg_column_mobile_error,
+                            (i+index_int),
+                            "手机号",
+                            mobile);
+                    strBuf.append(str_error);
+
+                    maxRow = maxRow + 1;
+                    if (maxShowRow_int <= maxRow) {return strBuf.toString();}
+                }
             }
 
         }
