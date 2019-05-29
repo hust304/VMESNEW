815ae96b5ad1ed6d35ca0d8a4b1e53416baa3d24
liuwei5815 <463681506@qq.com>
2019-05-29 17:18:17 +0800

    框架优化

--- vmes-server/src/main/java/com/xy/vmes/StartMain.java
+++ vmes-server/src/main/java/com/xy/vmes/StartMain.java
@@ -48,9 +48,9 @@ public class StartMain {
     public Connector httpConnector(){
         Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
-        connector.setPort(80);
+        connector.setPort(8080);
         connector.setSecure(false);
-        connector.setRedirectPort(443);
+        connector.setRedirectPort(8443);
         return connector;
     }
 
