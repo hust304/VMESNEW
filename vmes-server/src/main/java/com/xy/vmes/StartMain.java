185efbf4cfc593e56e09ba9d0af4ca945eaa9e7d
liuwei5815 <463681506@qq.com>
2019-05-30 11:46:36 +0800

    框架优化

--- vmes-server/src/main/java/com/xy/vmes/StartMain.java
+++ vmes-server/src/main/java/com/xy/vmes/StartMain.java
@@ -27,32 +27,32 @@ import javax.jms.Queue;
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class StartMain {
 
-    @Bean
-    public EmbeddedServletContainerFactory servletContainer(){
-        TomcatEmbeddedServletContainerFactory tomcat=new TomcatEmbeddedServletContainerFactory(){
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint=new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");//confidential
-                SecurityCollection collection=new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(httpConnector());
-        return tomcat;
-    }
-
-    @Bean
-    public Connector httpConnector(){
-        Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        connector.setPort(8088);
-        connector.setSecure(false);
-        connector.setRedirectPort(8080);
-        return connector;
-    }
+//    @Bean
+//    public EmbeddedServletContainerFactory servletContainer(){
+//        TomcatEmbeddedServletContainerFactory tomcat=new TomcatEmbeddedServletContainerFactory(){
+//            @Override
+//            protected void postProcessContext(Context context) {
+//                SecurityConstraint securityConstraint=new SecurityConstraint();
+//                securityConstraint.setUserConstraint("CONFIDENTIAL");//confidential
+//                SecurityCollection collection=new SecurityCollection();
+//                collection.addPattern("/*");
+//                securityConstraint.addCollection(collection);
+//                context.addConstraint(securityConstraint);
+//            }
+//        };
+//        tomcat.addAdditionalTomcatConnectors(httpConnector());
+//        return tomcat;
+//    }
+//
+//    @Bean
+//    public Connector httpConnector(){
+//        Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setScheme("http");
+//        connector.setPort(8088);
+//        connector.setSecure(false);
+//        connector.setRedirectPort(8080);
+//        return connector;
+//    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication application = new SpringApplication(StartMain.class);
