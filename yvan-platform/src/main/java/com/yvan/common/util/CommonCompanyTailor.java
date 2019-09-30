181ea8978daac33bcecf0738a0efd8b79bba69fe
ChenGang7906 <chen_gang7906@163.com>
2019-09-30 12:52:42 +0800

    获取当前企业是否定制化-通过企业管理员角色

--- yvan-platform/src/main/java/com/yvan/common/util/CommonCompanyTailor.java
+++ yvan-platform/src/main/java/com/yvan/common/util/CommonCompanyTailor.java
@@ -4,13 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 企业定制常量类
+ * 企业定制化配置常量类
  */
 public class CommonCompanyTailor {
-    //企业管理员角色id
-    public static final Map<String, String> SYS_COMPANY_ADMIN_ROLE = new HashMap<String, String>() {{
+    //通过企业管理员角色-配置企业定制化
+    public static final Map<String, String> SYS_COMPANY_ROLE = new HashMap<String, String>() {{
         //套餐F:(仓库(简版)-文成-含有货品价格) 角色id: e0af062de24046a385f9319dbad8d875
         //定制功能: 仓库(简版)-含有货品价格(文成)
         put("e0af062de24046a385f9319dbad8d875", "e0af062de24046a385f9319dbad8d875");
     }};
+
+
 }
