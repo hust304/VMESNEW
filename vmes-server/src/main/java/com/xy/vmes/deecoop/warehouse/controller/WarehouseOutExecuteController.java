69b34db91bdd43a63e9a226aa13ebeb0aaf05014
ChenGang7906 <chen_gang7906@163.com>
2019-10-22 15:05:56 +0800

    按出库执行记录退库操作

--- vmes-server/src/main/java/com/xy/vmes/deecoop/warehouse/controller/WarehouseOutExecuteController.java
+++ vmes-server/src/main/java/com/xy/vmes/deecoop/warehouse/controller/WarehouseOutExecuteController.java
@@ -1,6 +1,7 @@
 package com.xy.vmes.deecoop.warehouse.controller;
 
 import com.baomidou.mybatisplus.plugins.pagination.Pagination;
+import com.xy.vmes.common.util.DateFormat;
 import com.xy.vmes.common.util.StringUtil;
 import com.xy.vmes.entity.*;
 import com.xy.vmes.service.*;
@@ -15,8 +16,9 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.apache.commons.lang.StringUtils;
 
+import java.math.BigDecimal;
+import java.text.MessageFormat;
 import java.util.*;
-
 
 
 /**
@@ -27,11 +29,22 @@ import java.util.*;
 @RestController
 @Slf4j
 public class WarehouseOutExecuteController {
-
     private Logger logger = LoggerFactory.getLogger(WarehouseOutExecuteController.class);
 
     @Autowired
+    private WarehouseOutService warehouseOutService;
+    @Autowired
+    private WarehouseOutDetailService warehouseOutDetailService;
+
+    @Autowired
+    private WarehouseOutExecutorService warehouseOutExecutorService;
+    @Autowired
     private WarehouseOutExecuteService warehouseOutExecuteService;
+    @Autowired
+    private WarehouseOutDetailExecuteService warehouseOutDetailExecuteService;
+
+    @Autowired
+    private WarehouseProductService warehouseProductService;
 
 
     /**
@@ -179,12 +192,7 @@ public class WarehouseOutExecuteController {
         return model;
     }
 
-
-
     /*****************************************************以上为自动生成代码禁止修改，请在下面添加业务代码**************************************************/
-
-
-
     /**
      * 出库编辑
      * @author 刘威
@@ -202,7 +210,6 @@ public class WarehouseOutExecuteController {
         logger.info("################/warehouseOutExecute/updateWarehouseOutExecute 执行结束 总耗时"+(endTime-startTime)+"ms ################# ");
         return model;
     }
-
 
     /**
      * 出库删除
@@ -222,8 +229,6 @@ public class WarehouseOutExecuteController {
         return model;
     }
 
-
-
     /**
      * 出库执行
      * @author 刘威
@@ -242,19 +247,12 @@ public class WarehouseOutExecuteController {
         return model;
     }
 
-
-
-
-
-
-
     /**
     * @author 刘威 自动创建，可以修改
     * @date 2018-11-01
     */
     @PostMapping("/warehouse/warehouseOutExecute/listPageWarehouseOutExecutes")
     public ResultModel listPageWarehouseOutExecutes()  throws Exception {
-
         logger.info("################warehouseOutExecute/listPageWarehouseOutExecutes 执行开始 ################# ");
         Long startTime = System.currentTimeMillis();
         PageData pd = HttpUtils.parsePageData();
@@ -262,6 +260,170 @@ public class WarehouseOutExecuteController {
         ResultModel model = warehouseOutExecuteService.listPageWarehouseOutExecutes(pd,pg);
         Long endTime = System.currentTimeMillis();
         logger.info("################warehouseOutExecute/listPageWarehouseOutExecutes 执行结束 总耗时"+(endTime-startTime)+"ms ################# ");
+        return model;
+    }
+
+    /**
+     * 出库单-出库执行-退单
+     * @author 陈刚
+     * @date 2019-10-22
+     * @throws Exception
+     */
+    @PostMapping("/warehouse/warehouseOutExecute/rebackWarehouseOutExecute")
+    @Transactional(rollbackFor=Exception.class)
+    public ResultModel rebackWarehouseOutExecute() throws Exception {
+        logger.info("################/warehouse/warehouseOutExecute/rebackWarehouseOutExecute 执行开始 ################# ");
+        Long startTime = System.currentTimeMillis();
+
+        ResultModel model = new ResultModel();
+        PageData pd = HttpUtils.parsePageData();
+
+        String remarkTemp = "退单原因:{0}";
+        String dateTimeStr = DateFormat.date2String(new Date(), DateFormat.DEFAULT_DATETIME_FORMAT);
+        String remark = pd.getString("remark");
+        if (remark == null || remark.trim().length() == 0) {
+            model.putCode(Integer.valueOf(1));
+            model.putMsg("退单原因为必填项不可为空！");
+            return model;
+        }
+        String remarkStr = MessageFormat.format(remarkTemp, remark);
+
+        String outExecuteId = pd.getString("outExecuteId");
+        if (outExecuteId == null || outExecuteId.trim().length() == 0) {
+            model.putCode(Integer.valueOf(1));
+            model.putMsg("出库执行id为空或空字符串！");
+            return model;
+        }
+
+        String productId = pd.getString("productId");
+        if (productId == null || productId.trim().length() == 0) {
+            model.putCode(Integer.valueOf(1));
+            model.putMsg("货品id为空或空字符串！");
+            return model;
+        }
+
+        String cuser = pd.getString("cuser");
+        String companyId = pd.getString("currentCompanyId");
+
+        WarehouseOutExecute outExecute = warehouseOutExecuteService.selectById(outExecuteId);
+        if (outExecute == null) {return model;}
+
+        //detailId 出库单明细id
+        String detailId = outExecute.getDetailId();
+        //执行人ID executorId
+        String executorId = outExecute.getExecutorId();
+        //仓库货品id
+        String warehouseProductId = outExecute.getWarehouseProductId();
+        //当前出库数量 count
+        BigDecimal count = BigDecimal.valueOf(0D);
+        if (outExecute.getCount() != null) {
+            count = outExecute.getCount();
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        Map columnMap = new HashMap();
+        //出库明细id
+        columnMap.put("detail_id", detailId);
+        //出库执行人id
+        columnMap.put("executor_id", executorId);
+        columnMap.put("isdisable","1");
+
+        //(出库明细id,出库执行人id) 查询(vmes_warehouse_out_executor)
+        List<WarehouseOutExecutor> warehouseOutExecutorList = warehouseOutExecutorService.selectByColumnMap(columnMap);
+        if (warehouseOutExecutorList != null && warehouseOutExecutorList.size() > 0) {
+            for (WarehouseOutExecutor executor : warehouseOutExecutorList) {
+                WarehouseOutExecutor editOutExecutor = new WarehouseOutExecutor();
+
+                editOutExecutor.setId(executor.getId());
+                editOutExecutor.setRemark(remarkStr);
+                editOutExecutor.setIsdisable("0");
+                warehouseOutExecutorService.update(editOutExecutor);
+            }
+        }
+
+        //修改当前出库执行
+        WarehouseOutExecute editOutExecute = new WarehouseOutExecute();
+        editOutExecute.setId(outExecuteId);
+        editOutExecute.setIsdisable("0");
+        editOutExecute.setRemark(remarkStr);
+        warehouseOutExecuteService.update(editOutExecute);
+
+        //修改仓库数量-仓库货品表变更
+        WarehouseProduct warehouseProduct = warehouseProductService.selectById(warehouseProductId);
+        if (warehouseProduct != null && count.doubleValue() != 0D) {
+            //库存变更日志
+            WarehouseLoginfo loginfo = new WarehouseLoginfo();
+            loginfo.setCompanyId(companyId);
+            loginfo.setCuser(cuser);
+            //operation 操作类型(add:添加 modify:修改 delete:删除 reback:退单)
+            loginfo.setOperation("reback");
+
+            //beforeCount 操作变更前数量(业务相关)
+            loginfo.setBeforeCount(count);
+            //afterCount 操作变更后数量(业务相关)
+            loginfo.setAfterCount(BigDecimal.ZERO);
+
+            loginfo.setExecuteId(outExecuteId);
+            loginfo.setDetailId(detailId);
+
+            String msgStr = warehouseProductService.outStockCount(warehouseProduct, count.negate(), loginfo);
+            if (msgStr != null && msgStr.trim().length() > 0) {
+                model.putCode(Integer.valueOf(1));
+                model.putMsg(msgStr);
+                return model;
+            }
+        }
+
+        // 返回出库单明细Map结构体
+        // <出库单明细id, 出库单明细执行Map>
+        //     出库单明细执行Map
+        //     outParentId:       出库单id
+        //     outDtlId:          出库单明细id
+        //     outDtlCount:       出库单明细出库数量
+        //     outDtlExecuteCount 出库单明细出库执行数量
+        Map<String, Map<String, Object>> outDetailExecuteMap = warehouseOutDetailExecuteService.findOutDetailExecuteMapByOutDtlIds(detailId);
+        if (outDetailExecuteMap != null && outDetailExecuteMap.get(detailId) != null) {
+            Map<String, Object> outDetailExecuteValue = outDetailExecuteMap.get(detailId);
+            //outDtlCount:       出库单明细出库数量
+            BigDecimal outDtlCount = BigDecimal.valueOf(0D);
+            if (outDetailExecuteValue.get("outDtlCount") != null) {
+                outDtlCount = (BigDecimal)outDetailExecuteValue.get("outDtlCount");
+            }
+
+            //outDtlExecuteCount 出库单明细出库执行数量
+            BigDecimal outDtlExecuteCount = BigDecimal.valueOf(0D);
+            if (outDetailExecuteValue.get("outDtlExecuteCount") != null) {
+                outDtlExecuteCount = (BigDecimal)outDetailExecuteValue.get("outDtlExecuteCount");
+            }
+
+            //获取出库单明细状态
+            //明细状态(0:待派单 1:执行中 2:已完成 -1.已取消)
+            String delState = new String();
+            if (outDtlExecuteCount.doubleValue() == 0) {
+                delState = "0";
+            } else if (outDtlExecuteCount.doubleValue() != 0
+                && (outDtlExecuteCount.doubleValue() < outDtlCount.doubleValue())
+            ) {
+                delState = "1";
+            } else if  (outDtlExecuteCount.doubleValue() != 0
+                && (outDtlCount.doubleValue() <= outDtlExecuteCount.doubleValue())
+            ) {
+                delState = "2";
+            }
+
+            if (delState != null && delState.length() > 0) {
+                WarehouseOutDetail editDetail = new WarehouseOutDetail();
+                editDetail.setId(detailId);
+                editDetail.setState(delState);
+                editDetail.setRemark(remarkStr);
+                warehouseOutDetailService.update(editDetail);
+
+                warehouseOutService.updateState(detailId);
+            }
+        }
+
+        Long endTime = System.currentTimeMillis();
+        logger.info("################/warehouse/warehouseOutExecute/rebackWarehouseOutExecute 执行结束 总耗时"+(endTime-startTime)+"ms ################# ");
         return model;
     }
 
