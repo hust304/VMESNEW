313d51dce8d46840b06086e305098d75cf096736
ChenGang7906 <chen_gang7906@163.com>
2019-11-05 20:03:39 +0800

    订单明细变更查询修改

--- vmes-server/src/main/java/com/xy/vmes/deecoop/sale/service/SaleOrderDetailByChangeServiceImp.java
+++ vmes-server/src/main/java/com/xy/vmes/deecoop/sale/service/SaleOrderDetailByChangeServiceImp.java
@@ -1,6 +1,6 @@
 package com.xy.vmes.deecoop.sale.service;
 
-import com.yvan.common.util.Common;
+import com.xy.vmes.common.util.ColumnUtil;
 import com.xy.vmes.deecoop.sale.dao.SaleOrderDetailByChangeMapper;
 import com.xy.vmes.entity.Column;
 import com.xy.vmes.service.ColumnService;
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
 import java.util.*;
 
 @Service
@@ -41,68 +40,22 @@ public class SaleOrderDetailByChangeServiceImp implements SaleOrderDetailByChang
         if (fieldCode != null && fieldCode.trim().length() > 0) {
             columnList = columnService.modifyColumnByFieldCode(fieldCode, columnList);
         }
-
-        List<LinkedHashMap> titlesList = new ArrayList<LinkedHashMap>();
-        List<String> titlesHideList = new ArrayList<String>();
-        Map<String, String> varModelMap = new HashMap<String, String>();
-        if(columnList!=null&&columnList.size()>0){
-            for (Column column : columnList) {
-                if(column!=null){
-                    if("0".equals(column.getIshide())){
-                        titlesHideList.add(column.getTitleKey());
-                    }
-                    LinkedHashMap titlesLinkedMap = new LinkedHashMap();
-                    titlesLinkedMap.put(column.getTitleKey(),column.getTitleName());
-                    varModelMap.put(column.getTitleKey(),"");
-                    titlesList.add(titlesLinkedMap);
-                }
-            }
-        }
-        Map result = new HashMap();
-        result.put("hideTitles",titlesHideList);
-        result.put("titles",titlesList);
+        Map<String, Object> titleMap = ColumnUtil.findTitleMapByColumnList(columnList);
 
         //设置查询排序
-        pageData.put("orderStr", "orderDtl.cdate asc");
         String orderStr = pageData.getString("orderStr");
         if (orderStr != null && orderStr.trim().length() > 0) {
             pageData.put("orderStr", orderStr);
         }
 
-        List<Map> varMapList = new ArrayList();
         List<Map> varList = this.listOrderDetaiByChange(pageData);
-        if(varList!=null&&varList.size()>0){
-            for(int i=0;i<varList.size();i++){
-                Map map = varList.get(i);
-                Map<String, String> varMap = new HashMap<String, String>();
-                varMap.putAll(varModelMap);
-                for (Map.Entry<String, String> entry : varMap.entrySet()) {
-                    varMap.put(entry.getKey(),map.get(entry.getKey())!=null?map.get(entry.getKey()).toString():"");
-                }
-                varMapList.add(varMap);
-            }
-        }
+        List<Map> varMapList = ColumnUtil.getVarMapList(varList,titleMap);
 
-        for (Map<String, String> mapObject : varMapList) {
-            //计价类型(1:先计价 2:后计价)
-            String priceType = mapObject.get("priceType");
-            if ("2".equals(priceType)) {
-                BigDecimal prod_productPrice = BigDecimal.valueOf(0D);
-                String prod_productPrice_str = mapObject.get("prod_productPrice");
-                if (prod_productPrice_str != null && prod_productPrice_str.trim().length() > 0) {
-                    try {
-                        prod_productPrice = new BigDecimal(prod_productPrice_str);
-                    } catch (NumberFormatException e) {
-                        e.printStackTrace();
-                    }
-                }
-                //四舍五入到2位小数
-                prod_productPrice = prod_productPrice.setScale(Common.SYS_NUMBER_FORMAT_DEFAULT, BigDecimal.ROUND_HALF_UP);
-                mapObject.put("productPrice", prod_productPrice.toString());
-            }
-        }
-
+        Map result = new HashMap();
+        result.put("hideTitles",titleMap.get("hideTitles"));
+        result.put("titles",titleMap.get("titles"));
         result.put("varList",varMapList);
+
         model.putResult(result);
         return model;
     }
