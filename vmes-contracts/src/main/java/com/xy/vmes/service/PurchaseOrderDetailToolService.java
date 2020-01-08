8171900c27ca63db8022fdf82965554e72dcbfd0
ChenGang7906 <chen_gang7906@163.com>
2020-01-08 15:58:56 +0800

    采购订单明细工具查询基础类修改

--- vmes-contracts/src/main/java/com/xy/vmes/service/PurchaseOrderDetailToolService.java
+++ vmes-contracts/src/main/java/com/xy/vmes/service/PurchaseOrderDetailToolService.java
@@ -1,6 +1,5 @@
 package com.xy.vmes.service;
 
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -28,21 +27,21 @@ public interface PurchaseOrderDetailToolService {
      */
     Map<String, Map<String, Object>> findPurchaseOrderDetailMap(String purchaseOrderId);
 
-    /**
-     * 获取(采购明细id,采购数量,签收数量,退货数量[已完成])Map<String, Map<String, Object>>结构体
-     * 根据(采购订单明细id)采购订单明细汇总查询
-     *
-     * Map<采购订单明细id, 采购明细Map<String, Object>>
-     * 采购明细Map<String, Object>
-     *     detailId: 采购订单明细id
-     *     parentId: 采购订单id
-     *     detailCount: 采购数量
-     *     signCount: 签收数量
-     *     retreatCount: 退货数量(已完成)
-     *     arriveCount: 到货数量:= 签收数量 - 退货数量(已完成)
-     *
-     * @param detailIds  采购订单明细Id(','逗号分隔的字符串)
-     * @return
-     */
-    Map<String, Map<String, Object>> findPurchaseOrderDetailMapByDetail(String detailIds);
+//    /**
+//     * 获取(采购明细id,采购数量,签收数量,退货数量[已完成])Map<String, Map<String, Object>>结构体
+//     * 根据(采购订单明细id)采购订单明细汇总查询
+//     *
+//     * Map<采购订单明细id, 采购明细Map<String, Object>>
+//     * 采购明细Map<String, Object>
+//     *     detailId: 采购订单明细id
+//     *     parentId: 采购订单id
+//     *     detailCount: 采购数量
+//     *     signCount: 签收数量
+//     *     retreatCount: 退货数量(已完成)
+//     *     arriveCount: 到货数量:= 签收数量 - 退货数量(已完成)
+//     *
+//     * @param detailIds  采购订单明细Id(','逗号分隔的字符串)
+//     * @return
+//     */
+//    Map<String, Map<String, Object>> findPurchaseOrderDetailMapByDetail(String detailIds);
 }
--- vmes-server/src/main/java/com/xy/vmes/deecoop/purchase/service/PurchaseOrderDetailToolServiceImp.java
+++ vmes-server/src/main/java/com/xy/vmes/deecoop/purchase/service/PurchaseOrderDetailToolServiceImp.java
@@ -1,6 +1,5 @@
 package com.xy.vmes.deecoop.purchase.service;
 
-import com.xy.vmes.common.util.StringUtil;
 import com.xy.vmes.deecoop.purchase.dao.PurchaseOrderDetailCollectMapper;
 import com.xy.vmes.service.PurchaseOrderDetailToolService;
 import com.yvan.PageData;
@@ -63,46 +62,46 @@ public class PurchaseOrderDetailToolServiceImp implements PurchaseOrderDetailToo
         return detailMap;
     }
 
-    /**
-     * 获取(采购明细id,采购数量,签收数量,退货数量[已完成])Map<String, Map<String, Object>>结构体
-     * 根据(采购订单明细id)采购订单明细汇总查询
-     *
-     * Map<采购订单明细id, 采购明细Map<String, Object>>
-     * 采购明细Map<String, Object>
-     *     detailId: 采购订单明细id
-     *     parentId: 采购订单id
-     *     detailCount: 采购数量
-     *     signCount: 签收数量
-     *     retreatCount: 退货数量(已完成)
-     *     arriveCount: 到货数量:= 签收数量 - 退货数量(已完成)
-     *
-     * @param detailIds  采购订单明细Id(','逗号分隔的字符串)
-     * @return
-     */
-    public Map<String, Map<String, Object>> findPurchaseOrderDetailMapByDetail(String detailIds) {
-        Map<String, Map<String, Object>> detailMap = new HashMap<String, Map<String, Object>>();
-        if (detailIds == null || detailIds.trim().length() == 0) {return detailMap;}
-
-        List<Map<String, Object>> mapList = new ArrayList<Map<String, Object>>();
-        try {
-            PageData findMap = new PageData();
-            detailIds = StringUtil.stringTrimSpace(detailIds);
-            detailIds = "'" + detailIds.replace(",", "','") + "'";
-            findMap.put("detailIds", detailIds);
-            mapList = purchaseOrderDetailCollectMapper.findPurchaseOrderDetailCollect(findMap);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if (mapList != null && mapList.size() > 0) {
-            for (Map<String, Object> objectMap : mapList) {
-                //detailId: 采购订单明细id
-                String detailId = (String)objectMap.get("detailId");
-                detailMap.put(detailId, objectMap);
-            }
-        }
-
-        return detailMap;
-    }
+//    /**
+//     * 获取(采购明细id,采购数量,签收数量,退货数量[已完成])Map<String, Map<String, Object>>结构体
+//     * 根据(采购订单明细id)采购订单明细汇总查询
+//     *
+//     * Map<采购订单明细id, 采购明细Map<String, Object>>
+//     * 采购明细Map<String, Object>
+//     *     detailId: 采购订单明细id
+//     *     parentId: 采购订单id
+//     *     detailCount: 采购数量
+//     *     signCount: 签收数量
+//     *     retreatCount: 退货数量(已完成)
+//     *     arriveCount: 到货数量:= 签收数量 - 退货数量(已完成)
+//     *
+//     * @param detailIds  采购订单明细Id(','逗号分隔的字符串)
+//     * @return
+//     */
+//    public Map<String, Map<String, Object>> findPurchaseOrderDetailMapByDetail(String detailIds) {
+//        Map<String, Map<String, Object>> detailMap = new HashMap<String, Map<String, Object>>();
+//        if (detailIds == null || detailIds.trim().length() == 0) {return detailMap;}
+//
+//        List<Map<String, Object>> mapList = new ArrayList<Map<String, Object>>();
+//        try {
+//            PageData findMap = new PageData();
+//            detailIds = StringUtil.stringTrimSpace(detailIds);
+//            detailIds = "'" + detailIds.replace(",", "','") + "'";
+//            findMap.put("detailIds", detailIds);
+//            mapList = purchaseOrderDetailCollectMapper.findPurchaseOrderDetailCollect(findMap);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        if (mapList != null && mapList.size() > 0) {
+//            for (Map<String, Object> objectMap : mapList) {
+//                //detailId: 采购订单明细id
+//                String detailId = (String)objectMap.get("detailId");
+//                detailMap.put(detailId, objectMap);
+//            }
+//        }
+//
+//        return detailMap;
+//    }
 
 }
