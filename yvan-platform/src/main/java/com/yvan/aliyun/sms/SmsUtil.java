d88ef538aad5a3a150efe93a1553cae7232d13a3
ChenGang7906 <chen_gang7906@163.com>
2020-01-15 12:07:15 +0800

    系统启动报错修改

--- yvan-platform/src/main/java/com/yvan/aliyun/sms/SmsUtil.java
+++ yvan-platform/src/main/java/com/yvan/aliyun/sms/SmsUtil.java
@@ -1,6 +1,6 @@
 package com.yvan.aliyun.sms;
 
-import com.aliyun.mns.model.Message;
+//import com.aliyun.mns.model.Message;
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.IAcsClient;
 import com.aliyuncs.dysmsapi.model.v20170525.QuerySendDetailsRequest;
