c60dc4bc1b73e8c120bf4c930e20eaef1f035fbc
ChenGang7906 <50002706@qq.com>
2020-02-20 21:24:31 +0800

    系统获取验证码

--- vmes-contracts/src/main/java/com/xy/vmes/service/UserLoginService.java
+++ vmes-contracts/src/main/java/com/xy/vmes/service/UserLoginService.java
@@ -3,8 +3,6 @@ package com.xy.vmes.service;
 import com.yvan.PageData;
 import com.yvan.springmvc.ResultModel;
 
-import java.util.Map;
-
 public interface UserLoginService {
 //    Map<String, Object> findRedisMap(String jsonString);
 
