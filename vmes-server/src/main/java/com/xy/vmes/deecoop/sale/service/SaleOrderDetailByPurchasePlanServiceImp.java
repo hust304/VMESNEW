77a7ab706a18d63a5cef5e2eb0ebd1dc41c0d7db
ChenGang7906 <50002706@qq.com>
2020-02-24 15:22:37 +0800

    新增采购计划-勾选销售订单-(计划数量)设定默认值

--- vmes-server/src/main/java/com/xy/vmes/deecoop/sale/service/SaleOrderDetailByPurchasePlanServiceImp.java
+++ vmes-server/src/main/java/com/xy/vmes/deecoop/sale/service/SaleOrderDetailByPurchasePlanServiceImp.java
@@ -2,6 +2,7 @@ package com.xy.vmes.deecoop.sale.service;
 
 import com.baomidou.mybatisplus.plugins.pagination.Pagination;
 import com.xy.vmes.common.util.ColumnUtil;
+import com.xy.vmes.common.util.EvaluateUtil;
 import com.xy.vmes.common.util.StringUtil;
 import com.xy.vmes.deecoop.sale.dao.SaleOrderDetailByPurchasePlanMapper;
 import com.xy.vmes.entity.Column;
@@ -14,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -94,8 +96,74 @@ public class SaleOrderDetailByPurchasePlanServiceImp implements SaleOrderDetailB
         if (varList != null && varList.size() > 0) {
             for(int i=0; i < varList.size(); i++){
                 Map<String, Object> objectMap = varList.get(i);
-                //purchasePlanCount 计划数量
-                objectMap.put("purchasePlanCount", "0.00");
+                //(n2p:计量转换计价)///////////////////////////////////////////////////////////////////////////////////////////
+                String n2pFormula = (String)objectMap.get("npFormula");
+
+                //n2pIsScale 是否需要四舍五入(Y:需要四舍五入 N:无需四舍五入)
+                String n2pIsScale = new String();
+                if (objectMap.get("n2pIsScale") != null) {
+                    n2pIsScale = objectMap.get("n2pIsScale").toString().trim();
+                }
+
+                //n2pDecimalCount 小数位数 (最小:0位 最大:4位)
+                Integer n2pDecimalCount = Integer.valueOf(2);
+                if (objectMap.get("n2pDecimalCount") != null) {
+                    n2pDecimalCount = (Integer)objectMap.get("n2pDecimalCount");
+                }
+                //(计量单位)货品数量///////////////////////////////////////////////////////////////////////////////////////////
+                //allowStockCount (计量单位)可用库存数量: 库存数量 - 货品锁库数量 + (销售订单)货品锁库数量
+                BigDecimal allowStockCount = BigDecimal.valueOf(0D);
+                if (objectMap.get("allowStockCount") != null) {
+                    allowStockCount = (BigDecimal)objectMap.get("allowStockCount");
+                }
+
+                //safetyCount 安全库存
+                BigDecimal safetyCount = BigDecimal.valueOf(0D);
+                if (objectMap.get("safetyCount") != null) {
+                    safetyCount = (BigDecimal)objectMap.get("safetyCount");
+                }
+
+                //(单据单位)货品数量///////////////////////////////////////////////////////////////////////////////////////////
+                //orderCount 订购数量
+                BigDecimal orderCount = BigDecimal.valueOf(0D);
+                if (objectMap.get("orderCount") != null) {
+                    orderCount = (BigDecimal)objectMap.get("orderCount");
+                }
+
+                //allowStockCount (计量单位)可用库存数量 -- (n2pFormula)计量单位转换计价单位
+                allowStockCount = EvaluateUtil.countFormulaN2P(allowStockCount, n2pFormula);
+                allowStockCount = StringUtil.scaleDecimal(allowStockCount, n2pIsScale, n2pDecimalCount);
+                objectMap.put("allowStockCount", allowStockCount.toString());
+
+                //safetyCount 安全库存 -- (n2pFormula)计量单位转换计价单位
+                safetyCount = EvaluateUtil.countFormulaN2P(safetyCount, n2pFormula);
+                safetyCount = StringUtil.scaleDecimal(safetyCount, n2pIsScale, n2pDecimalCount);
+                objectMap.put("safetyCount", safetyCount.toString());
+
+                //purchasePlanCount 计划数量///////////////////////////////////////////////////////////////////////////////////////////
+                BigDecimal purchasePlanCount = BigDecimal.valueOf(0D);
+
+                //safetyCount 安全库存(0)
+                if (safetyCount.doubleValue() == 0D) {
+                    //purchasePlanCount 计划数量 := 可用库存数量 - 订购数量
+                    BigDecimal bigTemp = BigDecimal.valueOf(allowStockCount.doubleValue() - orderCount.doubleValue());
+                    if (bigTemp.doubleValue() < 0) {
+                        purchasePlanCount = BigDecimal.valueOf(bigTemp.doubleValue() * -1);
+                    } else if (bigTemp.doubleValue() >= 0) {
+                        purchasePlanCount = orderCount;
+                    }
+                } else if (safetyCount.doubleValue() > 0D) {
+                    //purchasePlanCount 计划数量 := (可用库存数量 - 订购数量) - 安全库存
+                    BigDecimal bigTemp = BigDecimal.valueOf(allowStockCount.doubleValue() - orderCount.doubleValue() - safetyCount.doubleValue());
+                    if (bigTemp.doubleValue() < 0) {
+                        purchasePlanCount = BigDecimal.valueOf(bigTemp.doubleValue() * -1);
+                    } else if (bigTemp.doubleValue() >= 0) {
+                        purchasePlanCount = orderCount;
+                    }
+                }
+
+                purchasePlanCount = StringUtil.scaleDecimal(purchasePlanCount, n2pIsScale, n2pDecimalCount);
+                objectMap.put("purchasePlanCount", purchasePlanCount.toString());
 
                 //isNeedPurchase 是否需要采购(0:不需要 1:需要)
                 String isNeedPurchase = new String("0");
