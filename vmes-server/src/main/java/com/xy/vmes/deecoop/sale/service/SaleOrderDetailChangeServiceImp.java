423694be60b9930ff4fb279941da2e345d44a2e8
ChenGang7906 <50002706@qq.com>
2020-03-14 17:04:44 +0800

    销售订单变更货品信息合并

--- vmes-server/src/main/java/com/xy/vmes/deecoop/sale/service/SaleOrderDetailChangeServiceImp.java
+++ vmes-server/src/main/java/com/xy/vmes/deecoop/sale/service/SaleOrderDetailChangeServiceImp.java
@@ -12,6 +12,7 @@ import com.xy.vmes.common.util.ColumnUtil;
 import com.xy.vmes.entity.Column;
 import com.xy.vmes.service.ColumnService;
 import com.xy.vmes.service.SaleOrderDetailService;
+import com.xy.vmes.service.SystemToolService;
 import com.yvan.HttpUtils;
 import com.yvan.PageData;
 import com.yvan.common.util.Common;
@@ -40,6 +41,8 @@ public class SaleOrderDetailChangeServiceImp implements SaleOrderDetailChangeSer
 
     @Autowired
     private ColumnService columnService;
+    @Autowired
+    private SystemToolService systemToolService;
 
     /**
      * 创建人：陈刚 自动创建，禁止修改
@@ -222,6 +225,12 @@ public class SaleOrderDetailChangeServiceImp implements SaleOrderDetailChangeSer
             return model;
         }
 
+        //addColumn 页面上传递需要添加的栏位
+        if (pd.get("addColumn") != null) {
+            Map<String, String> addColumnMap = (Map<String, String>) pd.get("addColumn");
+            ColumnUtil.addColumnByColumnList(columnList, addColumnMap);
+        }
+
         //获取指定栏位字符串-重新调整List<Column>
         String fieldCode = pd.getString("fieldCode");
         if (fieldCode != null && fieldCode.trim().length() > 0) {
@@ -246,7 +255,15 @@ public class SaleOrderDetailChangeServiceImp implements SaleOrderDetailChangeSer
             result.put("pageData", pg);
         }
 
-        List<Map> varList = this.getDataListPage(pd,pg);
+        List<Map> varList = this.getDataListPage(pd, pg);
+        if (varList != null && varList.size() > 0) {
+            //prodColumnKey 业务模块栏位key(','分隔的字符串)-顺序必须按(货品编码,货品名称,规格型号,货品自定义属性)摆放
+            String prodColumnKey = pd.getString("prodColumnKey");
+            for (Map<String, Object> mapObject : varList) {
+                String prodInfo = systemToolService.findProductInfo(prodColumnKey, mapObject);
+                mapObject.put("prodInfo", prodInfo);
+            }
+        }
         List<Map> varMapList = ColumnUtil.getVarMapList(varList,titleMap);
 
         result.put("hideTitles",titleMap.get("hideTitles"));
