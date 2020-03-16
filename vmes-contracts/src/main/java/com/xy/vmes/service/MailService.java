ef0400facc302e0d227d88dfa1f78c3e2725beae
ChenGang7906 <50002706@qq.com>
2020-03-16 14:12:59 +0800

    添加按Html格式发送邮件

--- vmes-contracts/src/main/java/com/xy/vmes/service/MailService.java
+++ vmes-contracts/src/main/java/com/xy/vmes/service/MailService.java
@@ -1,7 +1,10 @@
 package com.xy.vmes.service;
 
+import javax.mail.MessagingException;
+
 public interface MailService {
     /**
+     * 内容按文本发送邮件
      *
      * @param subject    邮件主题
      * @param content    邮件内容
@@ -9,4 +12,14 @@ public interface MailService {
      * @param ccPeoples  抄送人 ';'分隔的字符串
      */
     void sendSimpleMail(String subject, String content, String toWho, String ccPeoples);
+
+    /**
+     * 内容按Html格式发送邮件
+     *
+     * @param subject    邮件主题
+     * @param content    邮件内容
+     * @param toWho      主送人 ';'分隔的字符串
+     * @param ccPeoples  抄送人 ';'分隔的字符串
+     */
+    void sendHtmlMail(String subject, String content, String toWho, String ccPeoples) throws MessagingException;
 }
--- vmes-server/src/main/java/com/xy/vmes/deecoop/system/service/MailServiceImp.java
+++ vmes-server/src/main/java/com/xy/vmes/deecoop/system/service/MailServiceImp.java
@@ -5,7 +5,11 @@ import com.yvan.common.util.Common;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Service
 public class MailServiceImp implements MailService {
@@ -13,6 +17,7 @@ public class MailServiceImp implements MailService {
     private JavaMailSender mailSender;
 
     /**
+     * 内容按文本发送邮件
      *
      * @param subject    邮件主题
      * @param content    邮件内容
@@ -38,6 +43,43 @@ public class MailServiceImp implements MailService {
         }
 
         mailSender.send(mailMessage);
+    }
+
+    /**
+     * 内容按Html格式发送邮件
+     *
+     * @param subject    邮件主题
+     * @param content    邮件内容
+     * @param toWho      主送人 ';'分隔的字符串
+     * @param ccPeoples  抄送人 ';'分隔的字符串
+     */
+    public void sendHtmlMail(String subject, String content, String toWho, String ccPeoples) throws MessagingException {
+        if (subject == null || subject.trim().length() == 0) {return;}
+        if (content == null || content.trim().length() == 0) {return;}
+        if (toWho == null || toWho.trim().length() == 0) {return;}
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(message, true);
+
+        //邮件主题
+        message.setSubject(subject);
+        //邮件内容
+        messageHelper.setText(content, true);
+        //邮件发送人
+        messageHelper.setFrom(Common.SYS_MAIL_SEND);
+
+        //主送人
+        String[] toArry = toWho.split("\\;");
+        messageHelper.setTo(toArry);//主送人 setTo(String[] to) 支持多人
+
+        //抄送人
+        if (ccPeoples != null && ccPeoples.trim().length() > 0) {
+            String[] ccArry = ccPeoples.split("\\;");
+            messageHelper.setCc(ccArry);//抄送人 setCc(String[] cc) 支持多人
+        }
+
+        //发送
+        mailSender.send(message);
     }
 
     public static void main(String[] args) throws Exception {
