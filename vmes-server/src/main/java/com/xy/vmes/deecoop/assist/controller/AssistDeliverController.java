31f48f65b83a572b43384f118905b6b1f6727d2f
ChenGang7906 <50002706@qq.com>
2020-04-29 11:21:18 +0800

    生成外协发货单功能

--- vmes-server/src/main/java/com/xy/vmes/deecoop/assist/controller/AssistDeliverController.java
+++ vmes-server/src/main/java/com/xy/vmes/deecoop/assist/controller/AssistDeliverController.java
@@ -1,9 +1,17 @@
 package com.xy.vmes.deecoop.assist.controller;
 
+import com.xy.vmes.entity.AssistDeliver;
+import com.xy.vmes.entity.AssistDeliverDetail;
+import com.xy.vmes.entity.AssistDeliverDetailChild;
+import com.xy.vmes.service.AssistDeliverDetailChildService;
+import com.xy.vmes.service.AssistDeliverDetailService;
 import com.xy.vmes.service.AssistDeliverService;
 
+import com.xy.vmes.service.CoderuleService;
 import com.yvan.HttpUtils;
 import com.yvan.PageData;
+import com.yvan.YvanUtil;
+import com.yvan.common.util.Common;
 import com.yvan.springmvc.ResultModel;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
@@ -11,6 +19,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 /**
 * 说明：vmes_assist_deliver:外协发货表Controller
@@ -23,7 +34,14 @@ public class AssistDeliverController {
     private Logger logger = LoggerFactory.getLogger(AssistDeliverController.class);
 
     @Autowired
-    private AssistDeliverService assistDeliverService;
+    private AssistDeliverService deliverService;
+    @Autowired
+    private AssistDeliverDetailService deliverDetailService;
+    @Autowired
+    private AssistDeliverDetailChildService deliverDetailChildService;
+
+    @Autowired
+    private CoderuleService coderuleService;
 
      /**
     * @author 陈刚 自动创建，可以修改
@@ -34,7 +52,7 @@ public class AssistDeliverController {
         logger.info("################/assist/assistDeliver/listPageAssistDeliver 执行开始 ################# ");
         Long startTime = System.currentTimeMillis();
         PageData pd = HttpUtils.parsePageData();
-        ResultModel model = assistDeliverService.listPageAssistDeliver(pd);
+        ResultModel model = deliverService.listPageAssistDeliver(pd);
         Long endTime = System.currentTimeMillis();
         logger.info("################/assist/assistDeliver/listPageAssistDeliver 执行结束 总耗时"+(endTime-startTime)+"ms ################# ");
         return model;
@@ -61,6 +79,12 @@ public class AssistDeliverController {
             model.putMsg("企业id为空或空字符串！");
             return model;
         }
+        String supplierId = pageData.getString("supplierId");
+        if (supplierId == null || supplierId.trim().length() == 0) {
+            model.putCode(Integer.valueOf(1));
+            model.putMsg("供应商id为空或空字符串！");
+            return model;
+        }
         String orderId = pageData.getString("orderId");
         if (orderId == null || orderId.trim().length() == 0) {
             model.putCode(Integer.valueOf(1));
@@ -80,6 +104,47 @@ public class AssistDeliverController {
             model.putCode(Integer.valueOf(1));
             model.putMsg("发货单明细原材料JsonStr为空或空字符串！");
             return model;
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //1. 添加外协发货单表
+        AssistDeliver addDeliver = new AssistDeliver();
+        addDeliver.setCuser(cuser);
+        addDeliver.setMakeId(cuser);
+        addDeliver.setCompanyId(companyID);
+        addDeliver.setOrderId(orderId);
+        addDeliver.setSupplierId(supplierId);
+
+        //外协发货编号
+        String code = coderuleService.createCoderCdateOnShortYearByDate(
+                companyID,
+                "vmes_assist_deliver",
+                "D",
+                Common.CODE_RULE_LENGTH_3);
+        addDeliver.setSysCode(code);
+
+        //state:状态(0:待发货 1:已发货 -1:已取消)
+        addDeliver.setState("0");
+        deliverService.save(addDeliver);
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //2. 添加外协发货明细表
+        Map<String, String> deliverDetailMap = (Map<String, String>) YvanUtil.jsonToMap(deliverDetailJsonStr);
+        AssistDeliverDetail deliverDetail = (AssistDeliverDetail)HttpUtils.pageData2Entity(deliverDetailMap, new AssistDeliverDetail());
+        deliverDetail.setParentId(addDeliver.getId());
+        deliverDetail.setState(addDeliver.getState());
+        deliverDetail.setCuser(addDeliver.getCuser());
+        deliverDetailService.save(deliverDetail);
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //3. 添加外协发货明细子表(原材料)
+        List<Map<String, String>> mapList = (List<Map<String, String>>) YvanUtil.jsonToList(deliverDetailChildJsonStr);
+        for (Map<String, String> mapObject : mapList) {
+            AssistDeliverDetailChild detailChild = (AssistDeliverDetailChild)HttpUtils.pageData2Entity(mapObject, new AssistDeliverDetailChild());
+            detailChild.setDeliverId(deliverDetail.getParentId());
+            detailChild.setDeliverDtlId(deliverDetail.getId());
+            detailChild.setCuser(deliverDetail.getCuser());
+            deliverDetailChildService.save(detailChild);
         }
 
         Long endTime = System.currentTimeMillis();
